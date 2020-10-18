0f931179dbbf38e9a1174fea4e33e906436bd577
julja83 <59063644+julja83@users.noreply.github.com>
2020-10-18 13:49:36 +0300

    Правка (4. Тесты для стороны прямоугольника)

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -5,25 +5,30 @@ import org.junit.Test;
 
 public class SqAreaTest {
 
-  private double out(int p, int k) {
-    return SqArea.square(p, k);
-  }
-
   @Test
   public void whenp6k2then2() {
     int expected = 2;
-    Assert.assertEquals(expected, out(6, 2), 0.01);
+    int p = 6;
+    int k = 2;
+    double out = SqArea.square(p, k);
+    Assert.assertEquals(expected, out, 0.01);
   }
 
   @Test
-  public void whenp10k5then3() {
+  public void whenp10k2then3() {
     int expected = 3;
-    Assert.assertEquals(expected, out(10, 2), 0.01);
+    int p = 10;
+    int k = 2;
+    double out = SqArea.square(p, k);
+    Assert.assertEquals(expected, out, 0.01);
   }
 
   @Test
-  public void whenp15k9then5() {
+  public void whenp15k3then5() {
     int expected = 5;
-    Assert.assertEquals(expected, out(15, 3), 0.01);
+    int p = 15;
+    int k = 3;
+    double out = SqArea.square(p, k);
+    Assert.assertEquals(expected, out, 0.01);
   }
 }
