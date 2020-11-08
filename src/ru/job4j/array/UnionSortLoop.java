c0e0950a42e2080cff0d2c5714079abe3555630f
julja83 <59063644+julja83@users.noreply.github.com>
2020-11-08 16:42:35 +0300

    Объединение отсортированного массива

--- src/ru/job4j/array/UnionSortLoop.java
+++ src/ru/job4j/array/UnionSortLoop.java
@@ -4,44 +4,15 @@ public class UnionSortLoop {
 
   public static int[] union(int[] left, int[] right) {
     int[] rsl = new int[left.length + right.length];
+    int leftIndex = 0;
+    int rightIndex = 0;
 
-    int indexRsl = 0;
-    int indexRight = 0;
-    int indexLeft = 0;
-    for (int i = 0; i < left.length; i++) {
-      for (int j = indexRight; j < right.length; j++) {
-        if (left[i] < right[j]) {
-          rsl[indexRsl] = left[i];
-          indexRsl++;
-          indexLeft++;
-          break;
-        }
-        if (left[i] == right[j]) {
-          rsl[indexRsl] = right[j];
-          rsl[indexRsl + 1] = left[i];
-          indexRsl = indexRsl + 2;
-          indexRight++;
-          indexLeft++;
-          break;
-        }
-        if (left[i] > right[j]) {
-          rsl[indexRsl] = right[j];
-          indexRsl++;
-          i--;
-          indexRight++;
-          break;
-        }
-      }
-    }
-    if (indexLeft < indexRight) {
-      for (int i = indexLeft; i < left.length; i++) {
-        rsl[indexRsl] = left[i];
-        indexRsl++;
-      }
-    } else {
-      for (int i = indexRight; i < right.length; i++) {
-        rsl[indexRsl] = right[i];
-        indexRsl++;
+    while (leftIndex + rightIndex < rsl.length) {
+      if (rightIndex == right.length
+          || leftIndex != left.length && left[leftIndex] < right[rightIndex]) {
+        rsl[leftIndex + rightIndex] = left[leftIndex++];
+      } else {
+        rsl[leftIndex + rightIndex] = right[rightIndex++];
       }
     }
     return rsl;
