6a69c0868cef2e3c2d2c5a24d36659905fc011b4
julja83 <59063644+julja83@users.noreply.github.com>
2021-02-25 06:18:26 +0300

    Правки 2. Идентичные тексты

--- src/ru/job4j/collection/UniqueText.java
+++ src/ru/job4j/collection/UniqueText.java
@@ -6,7 +6,6 @@ public class UniqueText {
 
   public static boolean isEquals(String originText, String duplicateText) {
     boolean rsl = true;
-    int counter = 0;
     String[] origin = originText.split(" ");
     String[] text = duplicateText.split(" ");
     HashSet<String> check = new HashSet<>();
@@ -14,12 +13,9 @@ public class UniqueText {
       check.add(o);
     }
     for (String t : text) {
-      if (check.contains(t)) {
-        counter++;
+      if (!check.contains(t)) {
+        rsl = false;
       }
-    }
-    if (counter != text.length) {
-      rsl = false;
     }
     return rsl;
   }
