0185acc36bf987771cf5fa05f4ca27824c7e22c6
julja83 <59063644+julja83@users.noreply.github.com>
2021-02-25 06:33:20 +0300

    3. Модели данных и HashSet.

---
+++ test/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.collection;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import org.junit.Test;
+
+public class NotifyAccountTest {
+  @Test
+  public void sent() {
+    List<Account> accounts = Arrays.asList(
+        new Account("123", "Petr Arsentev", "eDer3432f"),
+        new Account("142", "Petr Arsentev", "000001")
+    );
+    HashSet<Account> expect = new HashSet<>(
+        Arrays.asList(
+            new Account("123", "Petr Arsentev", "eDer3432f"),
+            new Account("142", "Petr Arsentev", "000001")
+        )
+    );
+    assertThat(NotifyAccount.sent(accounts), is(expect));
+  }
+
+  @Test
+  public void sentSimilarPassport() {
+    List<Account> accounts = Arrays.asList(
+        new Account("123", "Petr Arsentev", "eDer3432f"),
+        new Account("142", "Petr Arsentev", "000001"),
+        new Account("142", "Petr Arsentev", "000002")
+    );
+    HashSet<Account> expect = new HashSet<>(
+        Arrays.asList(
+            new Account("123", "Petr Arsentev", "eDer3432f"),
+            new Account("142", "Petr Arsentev", "000001")
+        )
+    );
+    assertThat(NotifyAccount.sent(accounts), is(expect));
+  }
+
+  @Test
+  public void sentSimilarDeposit() {
+    List<Account> accounts = Arrays.asList(
+        new Account("123", "Petr Arsentev", "eDer3432f"),
+        new Account("142", "Petr Arsentev", "000001"),
+        new Account("147", "Petr Arsentev", "000001")
+    );
+    HashSet<Account> expect = new HashSet<>(
+        Arrays.asList(
+            new Account("123", "Petr Arsentev", "eDer3432f"),
+            new Account("142", "Petr Arsentev", "000001"),
+            new Account("147", "Petr Arsentev", "000001")
+        )
+    );
+    assertThat(NotifyAccount.sent(accounts), is(expect));
+  }
+}
