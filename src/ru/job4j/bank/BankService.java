f7df3ad1270482d9ea3abe3ce8a6799a0be7b27b
julja83 <59063644+julja83@users.noreply.github.com>
2021-03-15 11:14:51 +0300

    Правки 3. Банковские переводы

--- src/ru/job4j/bank/BankService.java
+++ src/ru/job4j/bank/BankService.java
@@ -50,7 +50,7 @@ public class BankService {
     boolean rsl = false;
     Account src = findByRequisite(srcPassport, srcRequisite);
     Account dest = findByRequisite(destPassport, destRequisite);
-    if ((src != null) && (dest != null) || (src.getBalance() > amount)) {
+    if (src != null && dest != null || src.getBalance() > amount) {
       src.setBalance(src.getBalance() - amount);
       dest.setBalance(dest.getBalance() + amount);
       rsl = true;
