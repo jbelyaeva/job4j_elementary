a5855b139385ac18c6d24fee14a1c79d249a47ed
julja83 <59063644+julja83@users.noreply.github.com>
2021-04-06 06:34:03 +0300

    Генератор текста через Map

--- src/ru/job4j/collection/Article.java
+++ src/ru/job4j/collection/Article.java
@@ -1,19 +1,20 @@
 package ru.job4j.collection;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Article {
 
   public static boolean generateBy(String origin, String line) {
     boolean rsl = true;
-    String[] originWords = origin.split(" |[.,:!]");
-    String[] text = line.split(" ");
-    HashSet<String> check = new HashSet<>();
-    for (String o : originWords) {
-      check.add(o);
+    String[] originText = origin.split(" |[.,:!]");
+    String[] newText = line.split(" ");
+    Map<String, String> check = new HashMap<>();
+    for (String o : originText) {
+      check.put(o, "");
     }
-    for (String t : text) {
-      if (!check.contains(t)) {
+    for (String t : newText) {
+      if (!check.containsKey(t)) {
         rsl = false;
         break;
       }
