bfad9c627bd7e7e5744f3a86e08c8dadaca4848d
julja83 <59063644+julja83@users.noreply.github.com>
2021-04-08 06:44:07 +0300

    1. Генератор текста. Правки

--- src/ru/job4j/collection/Article.java
+++ src/ru/job4j/collection/Article.java
@@ -1,38 +1,23 @@
 package ru.job4j.collection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 public class Article {
 
   public static boolean generateBy(String origin, String line) {
-    String[] originText = origin.split(" |[.,:!]");
-    String[] newText = line.split(" ");
-    Map<String, Integer> map1 = new HashMap<String, Integer>();
-    Map<String, Integer> map2 = new HashMap<String, Integer>();
-    for (String o : originText) {
-      int value = 1;
-      if (map1.containsKey(o)) {
-        value = map1.get(o);
-        value++;
-      }
-      map1.put(o, value);
+    boolean rsl = true;
+    String[] originWords = origin.replaceAll("\\p{P}", "").split(" ");
+    String[] text = line.replaceAll("\\p{P}", "").split(" ");
+    HashSet<String> check = new HashSet<>();
+    for (String o : originWords) {
+      check.add(o);
     }
-
-    for (String n : newText) {
-      int value = 1;
-      if (map2.containsKey(n)) {
-        value = map2.get(n);
-        value++;
-      }
-      map2.put(n, value);
-    }
-
-    for (String s : map2.keySet()) {
-      if (!map1.containsKey(s)) {
-        return false;
+    for (String t : text) {
+      if (!check.contains(t)) {
+        rsl = false;
+        break;
       }
     }
-    return true;
+    return rsl;
   }
 }
