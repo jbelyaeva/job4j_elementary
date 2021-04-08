b83fdfddaf27f52171c4b94a83385fe6e54603d6
julja83 <59063644+julja83@users.noreply.github.com>
2021-04-08 07:01:31 +0300

    2. Проверить две строчки на идентичность. Правки

--- src/ru/job4j/collection/FreezeStr.java
+++ src/ru/job4j/collection/FreezeStr.java
@@ -8,33 +8,30 @@ public class FreezeStr {
   public static boolean eq(String left, String right) {
     char[] leftArray = left.toCharArray();
     char[] rightArray = right.toCharArray();
-    Map<Character, Integer> map1 = new HashMap<Character, Integer>();
-    Map<Character, Integer> map2 = new HashMap<Character, Integer>();
+    Map<Character, Integer> map = new HashMap<Character, Integer>();
     for (char l : leftArray) {
       int value = 1;
-      if (map1.containsKey(l)) {
-        value = map1.get(l);
+      if (map.containsKey(l)) {
+        value = map.get(l);
         value++;
       }
-      map1.put(l, value);
+      map.put(l, value);
     }
-
     for (char r : rightArray) {
-      int value = 1;
-      if (map2.containsKey(r)) {
-        value = map2.get(r);
-        value++;
-      }
-      map2.put(r, value);
-    }
-
-    if (map1.size() != map2.size()) {
-      return false;
-    }
-    for (char c : map1.keySet()) {
-      if (!map2.containsKey(c) || map1.get(c) != map2.get(c)) {
+      if (!map.containsKey(r)) {
         return false;
       }
+      if (map.containsKey(r)) {
+        int value = map.get(r);
+        if (value == 1) {
+          map.remove(r);
+        } else {
+          map.put(r, value - 1);
+        }
+      }
+    }
+    if (!map.isEmpty()) {
+      return false;
     }
     return true;
   }
