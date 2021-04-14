07918143de13b596e297df38d3d791df7d8ee217
julja83 <59063644+julja83@users.noreply.github.com>
2021-04-14 06:56:33 +0300

    Правки 2.0. Встроенные функциональные интерфейсы.

--- src/ru/job4j/function/SearchFolder.java
+++ src/ru/job4j/function/SearchFolder.java
@@ -2,21 +2,15 @@ package ru.job4j.function;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class SearchFolder {
 
   public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
     List<Folder> result = new ArrayList<>();
-    Function<Folder, List<Folder>> func = (f) -> {
-      result.add(f);
-      return result;
-    };
-
     for (Folder f : list) {
       if (pred.test(f)) {
-        func.apply(f);
+       result.add(f);
       }
     }
     return result;
