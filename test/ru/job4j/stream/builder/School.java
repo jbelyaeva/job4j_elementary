e3df20caed48e914e1618cd1904e56018aaeb79c
julja83 <59063644+julja83@users.noreply.github.com>
2021-05-04 15:00:51 +0300

    9. Шаблоны проектирования в Stream API

---
+++ test/ru/job4j/stream/builder/School.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream.builder;
+
+import ru.job4j.stream.builder.Student.Builder;
+
+public class School {
+
+  public static void main(String[] args) {
+
+    Student student = new Builder().withName("Вася")
+        .withSurname("Васин")
+        .withAge(12)
+        .withIdFamily(1)
+        .withTz("Europe/Moscow")
+        .withCountry("Russia")
+        .withSkills(new int[]{1, 2, 3})
+        .withEmail("mail@MAIL.RU")
+        .build();
+    System.out.println(student);
+  }
+}
---
+++ test/ru/job4j/stream/builder/Student.java
@@ -0,0 +1,94 @@
+package ru.job4j.stream.builder;
+
+import java.util.Arrays;
+
+public class Student {
+
+  private String name;
+  private String surname;
+  private int age;
+  private int idFamily;
+  private String tz;
+  private String country;
+  private int[] skills;
+  private String email;
+
+  static class Builder {
+
+    private String name;
+    private String surname;
+    private int age;
+    private int idFamily;
+    private String tz;
+    private String country;
+    private int[] skills;
+    private String email;
+
+    Builder withName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    Builder withSurname(String surname) {
+      this.surname = surname;
+      return this;
+    }
+
+    Builder withAge(int age) {
+      this.age = age;
+      return this;
+    }
+
+    Builder withIdFamily(int idFamily) {
+      this.idFamily = idFamily;
+      return this;
+    }
+
+    Builder withTz(String tz) {
+      this.tz = tz;
+      return this;
+    }
+
+    Builder withCountry(String country) {
+      this.country = country;
+      return this;
+    }
+
+    Builder withSkills(int[] skills) {
+      this.skills = skills;
+      return this;
+    }
+
+    Builder withEmail(String email) {
+      this.email = email;
+      return this;
+    }
+
+    Student build() {
+      Student student = new Student();
+      student.name = name;
+      student.surname = surname;
+      student.age = age;
+      student.idFamily = idFamily;
+      student.tz = tz;
+      student.country = country;
+      student.skills = skills;
+      student.email = email;
+      return student;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "name='" + name + '\'' +
+        ", surname='" + surname + '\'' +
+        ", age=" + age +
+        ", idFamily=" + idFamily +
+        ", tz='" + tz + '\'' +
+        ", country='" + country + '\'' +
+        ", skills=" + Arrays.toString(skills) +
+        ", email='" + email + '\'' +
+        '}';
+  }
+}
